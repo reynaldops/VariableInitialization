0ea4d7650a39a9bdf329e0c45f5c5596aafed9b1
Reynaldo Perez Sanchez <Reynaldo@LAPTOP-66INHA92>
2017-08-21 22:13:11 -0300

    File commit - first code

---
+++ VariableInitialization.java
@@ -0,0 +1,16 @@
+package org.tutoriais;
+
+public class VariableInitializationChallenge {
+
+	static int secondNumber;
+
+	public static void main(String[] args) {
+		int firstNumber = 13;
+//		int secondNumber = 5;
+		int thirtNumber = 3;
+		
+		System.out.println((firstNumber++) + (secondNumber++) + (thirtNumber++));
+
+	}
+
+}
